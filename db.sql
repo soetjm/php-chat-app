9e67264bedb701b4e16ec9fc891e20ea667d2bae
soetjm <sofoniyaskediryesuf@gmail.com>
2025-01-11 03:28:11 -0800

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,16 +1,1 @@
-CREATE TABLE messages(
-    id BIGINT ,
-    sender BIGINT,
-    receiver BIGINT,
-    message TEXT,
-    files TEXT,
-    date DATETIME,
-    seen INT,
-    received INT,
-    deleted_sender TINYINT 1,
-    deleted_receiver TINYINT 1
-)
 
-
-
-RR!6D3CW&7pGia8
